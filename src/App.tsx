73f8102c1cb4d65ad357befe6776cb2f919ad8f4
Romane Golfetto <rgolfett@z4r4p2.42lyon.fr>
2025-11-28 11:25:16 +0100

    create repository

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Box } from './Box'
+//import { Librairie } from './section/Librairie'
+
+function App() {
+
+  return (
+    <div> 
+      <Box name="hello World"/>
+    </div>
+  )
+}
+
+export default App
