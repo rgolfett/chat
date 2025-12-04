865f3f71651d78b768dad86e6a2f09ca98da7611
Romane Golfetto <rgolfett@z2r2p8.42lyon.fr>
2025-12-04 13:19:53 +0100

    modify chat

--- src/sockets.ts
+++ src/sockets.ts
@@ -1,9 +1,19 @@
 
+// import { io } from "socket.io-client";
+
+// const URL = "http://localhost:3000";
+
+
+// export const socket = io(URL, {
+//   autoConnect: true,
+// });
+
+// src/socket.ts
+
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
-
-
+const URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:3001";
 export const socket = io(URL, {
   autoConnect: true,
 });
+
